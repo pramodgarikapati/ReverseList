8137a2a6e95cbc20c32e1f06f7ee6231e9f13328
pramo <pramo@DESKTOP-VP26RKU>
2024-07-11 12:26:57 -0400

    Add project files.

---
+++ ReverseList/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+reverseList(list);
+
+static void reverseList(List<int> list)
+{
+    for (int i = list.Count-1; i >= 0; i--)
+        Console.WriteLine(list[i]);
+    Console.ReadLine();
+}
+
